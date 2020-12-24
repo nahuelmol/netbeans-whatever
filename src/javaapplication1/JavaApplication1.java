415caf0b551df7147cb28bec56be03634e57d7da
Silvio <molinahuel44@gmail.com>
2020-12-24 19:17:32 -0300

    commit for upload what i did just for proof how it works, i mean how netbeans works

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,17 @@
+package javaapplication1;
+
+import com.sun.nio.zipfs.ZipInfo;
+import saludador.Main;
+
+public class JavaApplication1 {
+    
+    
+    public static void main(String[] args) {
+        int first = 14, second = 6;
+        
+        Main.Adios();
+        Main.Chiau();
+        Main.calculator(first,second);
+        
+    }
+}
---
+++ src/saludador/Main.java
@@ -0,0 +1,17 @@
+package saludador;
+
+public class Main {
+    public static void Chiau(){
+        System.out.println("HOla");
+    }
+    
+    public static void Adios(){
+        System.out.println("Adios");
+    }
+    
+    public static void calculator(int first,int second){
+        int result = 0;
+        result = first + second;
+        System.out.println("The result is: "+result);
+    }
+}
